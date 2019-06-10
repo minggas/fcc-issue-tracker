e19ec2ad5d051dfba0c0291408e41a8630e4ede8
minggas <fasfora@gmail.com>
2019-06-10 17:34:11 -0300

    fix: require Issue model

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+var Issue = require('../models/Issue').Issue;
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
